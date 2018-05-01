045ad8a181a07b8b82a9e7f0c2db0b3848fab208
Wyyx <gnehcyx@163.com>
2018-05-01 11:59:27 +0800

    make a theme

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute, Router } from '@angular/router'
+import { Component, OnInit, AfterViewInit } from '@angular/core'
 import { AuthService } from 'shared/services/auth.service'
 import { ShoppingCartService } from 'shared/services/shopping-cart.service'
 import { ShoppingCart } from 'shared/models/shopping-cart'
@@ -10,5 +11,12 @@ import { Observable } from 'rxjs'
 	styleUrls: [ './navbar.component.css' ]
 })
 export class NavbarComponent {
-	constructor(private authService: AuthService, private cartService: ShoppingCartService) {}
+	showLogin: boolean = true
+	constructor(
+		private authService: AuthService,
+		private cartService: ShoppingCartService,
+		private router: Router
+	) {
+		if (this.router.url === 'login') this.showLogin = false
+	}
 }
