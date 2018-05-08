a08f835129160b44b5ab9f7aac3c3b07004fbd49
Wyyx <gnehcyx@163.com>
2018-05-08 17:29:58 +0800

    change folders name

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 
 import { AppModule } from './app/app.module'
 import { environment } from './environments/environment'
-import './js/svg-with-js/js/fontawesome-all.js'
+import './js/fontawesome-svg-with-js/js/fontawesome-all.js'
 
 if (environment.production) {
 	enableProdMode()
